2b8b12605d0a2675603e1a7dd5ac364c33667a4a
LLAYUAN <2235901996@qq.com>
2024-03-19 23:13:31 +0800

    first time use JS

---
+++ src/bookstoreShopping/bookstoreShopping.js
@@ -0,0 +1,7 @@
+function clickCSAPP(){
+    window.location.href = 'http://localhost:63342/BookstoreFront/src/detailedPage_CSAPP/detailedPage_CSAPP.html?_ijt=ul1l506ghd7ocdarv07pnil8rd';
+}
+
+function clickThreeBody(){
+    window.location.href = 'http://localhost:63342/BookstoreFront/src/detailedPage_threeBody/detailedPage_threeBody.html?_ijt=sovb51okol8og8rvgjul0kgkit';
+}
---
+++ src/login/index.js
@@ -0,0 +1,3 @@
+function clickLoginBtn(){
+    window.location.href = 'http://localhost:63342/BookstoreFront/src/bookstoreShopping/bookstoreShopping.html?_ijt=og2veqogd5ka26d1v0td8q4m11';
+}
